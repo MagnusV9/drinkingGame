f4790fa0c66e730e9490308eac8e36762c2111a6
magnu <magnus.vestvik@student.uib.no>
2022-06-12 16:51:10 +0200

    coded some logic for changing activity.

--- app/src/main/java/com/example/dicerolldrinkinggame/MainActivity.kt
+++ app/src/main/java/com/example/dicerolldrinkinggame/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.animation.AnimationUtils
+import android.widget.Button
 import android.widget.ImageButton
+import android.widget.TextView
 import dice.Dice
 
 class MainActivity : AppCompatActivity() {
@@ -14,43 +16,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.game_screen)
         //val allScreens = mapOf("gameScreen" to R.layout.game_screen, "homeScreen" to R.layout.home_screen, "winScreen" to R.layout.win_screen)
-        val diceRoller: ImageButton = findViewById(R.id.diceButton)
-        val animation = AnimationUtils.loadAnimation(this,R.anim.jiggle)
-        val mediaPlayer: MediaPlayer = MediaPlayer.create(this, R.raw.opus_remix)
-        // lag en timer for hva som skal skje når du har vunnet
-        // link til brukt sang : https://www.youtube.com/watch?v=_lAsGw7evcw&ab_channel=AlexanderPeraltaArevalo
-        val timer = object: CountDownTimer(350,350){
-            override fun onTick(millisUntilFinished: Long) {
-                diceRoller.startAnimation(animation)
-            }
-            override fun onFinish() {
-                diceRoller.clearAnimation()
-            }
-
-        }
-
-        diceRoller.setOnClickListener {
-            mediaPlayer.start()
-            mediaPlayer.setVolume(100f,100f)
-            timer.start()
-            val rolledDice: Int = Dice().rollDice()
-            val drawableResource = when (rolledDice){
-                1 -> R.drawable.dice_1
-                2 -> R.drawable.dice_2
-                3 -> R.drawable.dice_3
-                4 -> R.drawable.dice_4
-                5 -> R.drawable.dice_5
-                else -> {
-                    R.drawable.dice_6
-                }
-            }
-            diceRoller.setImageResource(drawableResource)
-            diceRoller.contentDescription = rolledDice.toString()
-
+        val playButton : Button = findViewById(R.id.play)
+        val howToPlayButton : Button = findViewById(R.id.howToPlay)
+        val playText :TextView = findViewById(R.id.play_description)
+        val menueButton : Button = findViewById(R.id.menue)
+        playButton.setOnClickListener(){
+            val game = gameActivity()
         }
 
     }
 }
+
+// kræsjer litt usikker på hvorfor.
 
 // https://stackoverflow.com/questions/7404995/android-switch-between-layouts
 // se link for kordan du bytte layout
