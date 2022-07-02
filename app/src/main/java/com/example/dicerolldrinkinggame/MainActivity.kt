049af54a52d6e8d6abf9a04d9a17636596b400ad
magnus <Magnus_95_vestvik@hotmail.com>
2022-07-02 20:39:22 +0200

    added restart button, does not yet function propely

--- app/src/main/java/com/example/dicerolldrinkinggame/MainActivity.kt
+++ app/src/main/java/com/example/dicerolldrinkinggame/MainActivity.kt
@@ -4,6 +4,7 @@ import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.ImageButton
@@ -12,13 +13,26 @@ import dice.Dice
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.game_screen)
+        setContentView(R.layout.activity_game)
         val diceRoller: ImageButton = findViewById(R.id.diceButton)
         val animation = AnimationUtils.loadAnimation(this,R.anim.jiggle)
         val mediaPlayer: MediaPlayer = MediaPlayer.create(this, R.raw.opus_remix)
         val restartButton : Button = findViewById(R.id.restart_button)
         // lag en timer for hva som skal skje når du har vunnet
         // link til brukt sang : https://www.youtube.com/watch?v=_lAsGw7evcw&ab_channel=AlexanderPeraltaArevalo
+        val gameOver = object : CountDownTimer(126000,126000){
+            override fun onTick(p0: Long) {
+
+            }
+            //on finish virker ikke som blir kallt
+            override fun onFinish() {
+                // her må du set visibility.
+               diceRoller.setVisibility(View.GONE)
+                println("yooo")
+            }
+
+        }
+
         val timer = object: CountDownTimer(350,350){
             override fun onTick(millisUntilFinished: Long) {
                 diceRoller.startAnimation(animation)
@@ -26,6 +40,7 @@ class MainActivity : AppCompatActivity() {
             override fun onFinish() {
                 diceRoller.clearAnimation()
             }
+
 
         }
         restartButton.setOnClickListener(){
@@ -38,6 +53,7 @@ class MainActivity : AppCompatActivity() {
                 mediaPlayer.start()
                 mediaPlayer.setVolume(100f, 100f)
                 timer.start()
+                gameOver.start()
 
             val rolledDice: Int = Dice().rollDice()
             val drawableResource = when (rolledDice){
