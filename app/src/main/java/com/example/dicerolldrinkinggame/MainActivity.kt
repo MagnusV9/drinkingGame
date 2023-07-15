8b99f7ca6bb29092b47000637f9eb43720262bab
Magnus <100690976+MagnusV9@users.noreply.github.com>
2023-07-16 00:03:34 +0200

    refactored code base

--- app/src/main/java/com/example/dicerolldrinkinggame/MainActivity.kt
+++ app/src/main/java/com/example/dicerolldrinkinggame/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.dicerolldrinkinggame
 
 import android.media.MediaPlayer
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.CountDownTimer
 import android.view.View
-import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import android.os.CountDownTimer
+import android.view.animation.Animation
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
@@ -13,92 +13,92 @@ import androidx.core.view.isVisible
 import dice.Dice
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var diceRoller: ImageButton
+    private lateinit var restartButton: Button
+    private lateinit var loseDescription: TextView
+    private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var animation: Animation
+    private var runningGameOverCounter = false
+
+    private val gameOverTimer = object : CountDownTimer(122600,122600) {
+        override fun onTick(p0: Long) {}
+        override fun onFinish() {
+            showGameOver()
+        }
+    }
+
+    private val rollDiceTimer = object: CountDownTimer(350,350) {
+        override fun onTick(millisUntilFinished: Long) {
+            diceRoller.startAnimation(animation)
+        }
+        override fun onFinish() {
+            diceRoller.clearAnimation()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
-        val diceRoller: ImageButton = findViewById(R.id.diceButton)
-        val animation = AnimationUtils.loadAnimation(this,R.anim.jiggle)
-        val mediaPlayer: MediaPlayer = MediaPlayer.create(this, R.raw.opus_remix)
-        val restartButton : Button = findViewById(R.id.restart_button)
-        val loseDescription : TextView = findViewById(R.id.lose_description)
-        var runningGameOverCounter = false;
-        // lag en timer for hva som skal skje når du har vunnet
-        // link til brukt sang : https://www.youtube.com/watch?v=_lAsGw7evcw&ab_channel=AlexanderPeraltaArevalo
-        val gameOver = object : CountDownTimer(122600,122600){
-            override fun onTick(p0: Long) {
 
-            }
-            override fun onFinish() {
-               diceRoller.setVisibility(View.GONE)
-                loseDescription.setVisibility(View.VISIBLE)
+        diceRoller = findViewById(R.id.diceButton)
+        restartButton = findViewById(R.id.restart_button)
+        loseDescription = findViewById(R.id.lose_description)
+        mediaPlayer = MediaPlayer.create(this, R.raw.opus_remix)
+        animation = AnimationUtils.loadAnimation(this, R.anim.jiggle)
 
+        setUpClickListeners()
+    }
 
-
-            }
-
-        }
-
-        val timer = object: CountDownTimer(350,350){
-            override fun onTick(millisUntilFinished: Long) {
-                diceRoller.startAnimation(animation)
-            }
-            override fun onFinish() {
-                diceRoller.clearAnimation()
-            }
-
-
-        }
-        restartButton.setOnClickListener(){
-            if (mediaPlayer.isPlaying) {
-                mediaPlayer.pause()
-                mediaPlayer.seekTo(0)
-            }
-            runningGameOverCounter = false
-            gameOver.cancel()
-            if(!diceRoller.isVisible)
-                diceRoller.setVisibility(View.VISIBLE)
-            if(loseDescription.isVisible)
-                loseDescription.setVisibility(View.GONE)
-
-        }
+    private fun setUpClickListeners() {
         diceRoller.setOnClickListener {
-            if(!mediaPlayer.isPlaying) {
-                mediaPlayer.start()
-                mediaPlayer.setVolume(100f, 100f)
-            }
-                timer.start()
-            if(!runningGameOverCounter){
-                gameOver.start()
-                runningGameOverCounter = true
-            }
-
-
-            val rolledDice: Int = Dice().rollDice()
-            val drawableResource = when (rolledDice){
-                1 -> R.drawable.dice_1
-                2 -> R.drawable.dice_2
-                3 -> R.drawable.dice_3
-                4 -> R.drawable.dice_4
-                5 -> R.drawable.dice_5
-                else -> {
-                    R.drawable.dice_6
-                }
-            }
-            diceRoller.setImageResource(drawableResource)
-            diceRoller.contentDescription = rolledDice.toString()
-
+            rollDice()
         }
+        restartButton.setOnClickListener {
+            restartGame()
+        }
+    }
 
+    private fun rollDice() {
+        if(!mediaPlayer.isPlaying) {
+            mediaPlayer.start()
+            mediaPlayer.setVolume(100f, 100f)
+        }
+        rollDiceTimer.start()
+        if(!runningGameOverCounter){
+            gameOverTimer.start()
+            runningGameOverCounter = true
+        }
+        val rolledDice: Int = Dice().rollDice()
+        val drawableResource = getDiceDrawableResource(rolledDice)
+        diceRoller.setImageResource(drawableResource)
+        diceRoller.contentDescription = rolledDice.toString()
+    }
+
+    private fun getDiceDrawableResource(diceNumber: Int) = when (diceNumber) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
+    private fun restartGame() {
+        if (mediaPlayer.isPlaying) {
+            mediaPlayer.pause()
+            mediaPlayer.seekTo(0)
+        }
+        runningGameOverCounter = false
+        gameOverTimer.cancel()
+        if(!diceRoller.isVisible)
+            diceRoller.setVisibility(View.VISIBLE)
+        if(loseDescription.isVisible)
+            loseDescription.setVisibility(View.GONE)
+    }
+
+    private fun showGameOver() {
+        diceRoller.setVisibility(View.GONE)
+        loseDescription.setVisibility(View.VISIBLE)
     }
 }
-
-// https://stackoverflow.com/questions/7404995/android-switch-between-layouts
-// se link for kordan du bytte layout
-
-// https://www.youtube.com/watch?v=2xyNawgIk7Q&ab_channel=ProgrammerWorld
-// se for hvordan du kan ha flere layouts
-
-// https://www.youtube.com/watch?v=I2cdYnCyoDk&ab_channel=CodingDemos
-// flere animasjoner.
-
-//https://developer.android.com/guide/fragments
